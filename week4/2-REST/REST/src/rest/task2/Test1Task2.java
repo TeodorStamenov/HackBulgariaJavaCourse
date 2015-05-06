edcf8b288f08c2ccce4e700b5e0f6caeaae69e33
Teodor Stamenov <teodor.m.stamenov@gmail.com>
2015-05-06 16:15:59 +0300

    Rest Json HttpClient

--- week4/2-REST/REST/src/rest/task2/Test1Task2.java
+++ week4/2-REST/REST/src/rest/task2/Test1Task2.java
@@ -1,27 +1,8 @@
 package rest.task2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-
 public class Test1Task2 {
 
-	public static void main(String[] args) throws IOException {
-		URL url = new URL("http://ebusiness.free.bg");
-		BufferedReader read = new BufferedReader(new InputStreamReader(url.openStream()));
-		String input;
-		StringBuilder strBuild = new StringBuilder();
-		while ((input = read.readLine()) != null) {
-			strBuild.append(input);
-		}
-		read.close();
-		List<String> str = WebCrawler.getAllLinks(strBuild.toString());
-		for (String s : str){
-			System.out.println(s);
-		}
+	public static void main(String[] args) {
+		WebCrawler.search("http://ebusiness.free.bg", "Револвираща");
 	}
-
 }
--- week4/2-REST/REST/src/rest/task2/WebCrawler.java
+++ week4/2-REST/REST/src/rest/task2/WebCrawler.java
@@ -3,39 +3,64 @@ package rest.task2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WebCrawler {
+	private static Stack<URL> stack = new Stack<URL>();
+	private static URL url;
 
-	public static List<String> getAllLinks(String content) {
-        ArrayList<String> resultList = new ArrayList<>();
-        String regex = "<a.*?href=\"((?!javascript).*?)\".*?>";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(content);
-        while (matcher.find()) {
-            resultList.add(matcher.group(1));
-        }
-        return resultList;
-    }
-	
-//	public static void main(String[] args) throws IOException {
-//			Queue<String> urls = new LinkedList<String>();
-//			URL test = new URL("http://ebusiness.free.bg/");
-//			BufferedReader br = new BufferedReader(new InputStreamReader(test.openStream()));
-//			String input;
-//			while ((input = br.readLine()) != null) {
-//				System.out.println(input);
-//			}
-//			br.close();
-//			List<String> list = getAllLinks(" ");
-//			System.out.println(list);
-//	}
+	public static void checkForLink(String content) {
+		URL tempUrl;
+		String regex = "<a.*?href=\"((?!javascript).*?)\".*?>";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(content);
+		if (matcher.find()) {
+			try {
+                if (!matcher.toString().contains("index.html") && !matcher.toString().contains("http://") && !matcher.toString().contains("https://")) {
+                	tempUrl = new URL(String.format("%s%s", url.toString() + "/", matcher.group(1)));
+                	stack.push(tempUrl);
+                }
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
+	public static void search(String webAddress, String needle) {
+		BufferedReader read = null;
+		String input;
+		try {
+			url = new URL(webAddress);
+			stack.push(url);
+			while (!stack.isEmpty()) {
+				url = stack.pop();
+				read = new BufferedReader(new InputStreamReader(
+						url.openStream(), "UTF-8"));
+				while ((input = read.readLine()) != null) {
+					if (input.indexOf(needle) != -1) {
+						System.out.println(url.toString());
+						stack.clear();
+						break;
+					} else {
+						checkForLink(input);
+					}
+				}
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				read.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
